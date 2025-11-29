373c17c9af175d9fc6439351fa907dde6b23254e
jerriker <befikerkassahuntessema@gmail.com>
2025-11-29 12:30:55 +0300

    feat: add functionality to display each tab panels

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggleBtn = document.getElementById('toggleBtn');
 const moreBrands = document.getElementById('moreBrands');
+const tabLinks = document.querySelectorAll('.tab-link a');
+const tabPanels = document.querySelectorAll('.tab-panel');
 
 toggleBtn.addEventListener('click', () => {
     if (moreBrands.style.display === 'grid') {
@@ -7,4 +9,19 @@ toggleBtn.addEventListener('click', () => {
     } else {
         moreBrands.style.display = 'grid';
     }
+});
+
+tabLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        tabLinks.forEach(tabLink => tabLink.classList.remove('active'));
+        tabPanels.forEach(panel => panel.style.display = 'none');
+
+        link.classList.add('active');;
+
+        const tabId = link.classList[0];
+        const targetPanel = document.getElementById(tabId);
+
+        if (targetPanel)
+            targetPanel.style.display = 'block';
+    });
 });

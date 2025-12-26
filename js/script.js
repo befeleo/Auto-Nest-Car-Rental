c3139f88eabb46db3059d6beb0a080d6431dc7ce
jerriker <befikerkassahuntessema@gmail.com>
2025-12-26 19:13:11 +0300

    fix: update id for more brands and add filtering method to display the brands in service page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+// --- 1. UI Logic (Tabs & Toggle) ---
 const toggleBtn = document.getElementById('toggleBtn');
-const moreBrands = document.getElementById('moreBrands');
+const moreBrands = document.getElementById('more-brands');
 const tabLinks = document.querySelectorAll('.tab-link a');
 const tabPanels = document.querySelectorAll('.tab-panel');
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn?.addEventListener('click', () => {
     moreBrands.style.display = (moreBrands.style.display === 'grid') ? 'none' : 'grid';
 });
 
@@ -13,14 +14,25 @@ tabLinks.forEach((link) => {
 
         tabLinks.forEach(tabLink => tabLink.parentElement.classList.remove('active'));
         tabPanels.forEach(panel => panel.style.display = 'none');
-
         link.parentElement.classList.add('active');
-
-        const tabId = link.classList[0];
+        const tabId = link.className; // Uses the class name as the ID
         const targetPanel = document.getElementById(tabId);
-
-        if (targetPanel) {
-            targetPanel.style.display = 'block';
-        }
+        if (targetPanel) targetPanel.style.display = 'block';
     });
 });
+
+const redirectToFilter = (value) => {
+    if (!value) return;
+    window.location.href = `services.html?brand=${encodeURIComponent(value)}`;
+};
+
+const brandCard = document.querySelectorAll('.brand-card');
+
+brandCard.forEach(brand => {
+    brand.addEventListener('click', () => {
+        const brandName = brand.getAttribute('data-brand')
+
+        redirectToFilter(brandName);
+    });
+});
+

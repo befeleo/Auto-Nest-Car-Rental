6f1b828b4ed58171c4a1b6f555bdef0a65e98ae2
jerriker <befikerkassahuntessema@gmail.com>
2025-12-31 09:45:16 +0300

    feat: add booking functionality

--- js/admin.js
+++ js/admin.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
             if (targetSection) {
                 e.preventDefault();
 
+
                 allSections.forEach(section => section.classList.remove('active-section'));
                 targetSection.classList.add('active-section');
             }
@@ -17,6 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// Inventory
 let inventory = [];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -24,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedData = localStorage.getItem('autoNestCars');
     if (savedData) {
         inventory = JSON.parse(savedData);
-        renderTable();
+        renderInventory();
     }
 
     const form = document.getElementById('add-car-form');
@@ -57,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
             inventory.push(newCar);
         }
 
-        save();
+        saveInventory();
         modal.style.display = 'none';
         form.reset();
     });
@@ -74,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-const renderTable = () => {
+const renderInventory = () => {
     const tbody = document.getElementById('admin-car-list');
     tbody.innerHTML = inventory.map(car => `
         <tr class="inventory-row">
@@ -85,10 +87,10 @@ const renderTable = () => {
             <td class="price-cell">${car.price.toLocaleString()} Birr</td>
             <td>
                 <div class="action-buttons">
-                    <button class="btn-edit" onclick="editCar(${car.id})">
+                    <button class="btn-edit" onclick="editInventory(${car.id})">
                         <img src="assets/icons/pen.svg" alt="edit"  class="action-img" />
                     </button>
-                    <button class="btn-delete" onclick="deleteCar(${car.id})"> 
+                    <button class="btn-delete" onclick="deleteInventory(${car.id})"> 
                         <img src="assets/icons/trash-can.svg" alt="delete" class="action-img" />
                     </button>
                 </div>
@@ -96,19 +98,19 @@ const renderTable = () => {
         </tr>`).join('');
 }
 
-const save = () => {
+const saveInventory = () => {
     localStorage.setItem('autoNestCars', JSON.stringify(inventory));
-    renderTable();
+    renderInventory();
 }
 
-const deleteCar = (id) => {
+const deleteInventory = (id) => {
     inventory = inventory.filter(car => car.id !== id);
-    save();
+    saveInventory();
 }
 
-window.deleteCar = deleteCar;
+window.deleteInventory = deleteInventory;
 
-const editCar = (id) => {
+const editInventory = (id) => {
     const car = inventory.find(c => c.id === id);
     if (!car) return;
 
@@ -129,4 +131,84 @@ const editCar = (id) => {
     document.getElementById('formModal').style.display = 'flex';
 };
 
-window.editCar = editCar;
+window.editInventory = editInventory;
+
+// Booking
+let bookings = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedBookings = localStorage.getItem('autoNestBookings');
+    if (savedBookings) {
+        bookings = JSON.parse(savedBookings);
+    }
+    else {
+        bookings = [
+            { id: 101, customerName: "Abebe", carName: "Toyota", phone: "0911223344", date: "2023-12-25", status: "Pending" },
+            { id: 102, customerName: "Biruk", carName: "Lada", phone: "0922334455", date: "2023-12-26", status: "Confirmed" },
+            { id: 103, customerName: "Kebede", carName: "Hyundai", phone: "0933445566", date: "2023-12-27", status: "Cancelled" },
+        ];
+        localStorage.setItem('autoNestBookings', JSON.stringify(bookings));
+    }
+    renderBookings();
+});
+
+const renderBookings = () => {
+    const tbody = document.getElementById('admin-booking-list');
+    if (!tbody) return;
+
+    tbody.innerHTML = bookings.map(book => {
+        return `
+            <tr class="inventory-row">
+                <td>${book.customerName}</td>
+                <td>${book.carName}</td>
+                <td>${book.phone}</td>
+                <td>${book.date}</td>
+                <td>
+                    <span class="status ${book.status.toLowerCase()}">
+                        ${book.status}
+                    </span>
+                </td>
+                <td>
+                    <div class="action-buttons">
+                        <button type="button" class="btn-confirm" onclick="updateStatus(${book.id}, 'Confirmed')">
+                            <img src="assets/icons/check.svg" alt="confirm" class="action-img" />
+                        </button>
+                        
+                        <button type="button" class="btn-cancel-status" onclick="updateStatus(${book.id}, 'Cancelled')">
+                            <img src="assets/icons/x-mark.svg" alt="cancel" class="action-img" />
+                        </button>
+
+                        <button type="button" class="btn-delete-perm" onclick="deleteBooking(${book.id})">
+                            <img src="assets/icons/trash-can.svg" alt="delete" class="action-img" />
+                        </button>
+                    </div>
+                </td>
+            </tr>`;
+    }).join('');
+};
+
+const saveBooking = () => {
+    localStorage.setItem('autoNestBookings', JSON.stringify(bookings));
+    renderBookings();
+};
+
+const updateStatus = (e, id, newStatus) => {
+    if (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+    const bookingIndex = bookings.findIndex(booking => Number(booking.id) === Number(id));
+    if (bookingIndex !== -1) {
+        bookings[bookingIndex].status = newStatus;
+        saveBooking();
+    }
+};
+
+const deleteBooking = (id) => {
+
+    bookings = bookings.filter(booking => Number(booking.id) !== Number(id));
+    saveBooking();
+};
+
+window.updateStatus = updateStatus;
+window.deleteBooking = deleteBooking;
